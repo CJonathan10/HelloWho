4239ac37699946235260863a5dbed38929eff255
Jonathan Orellana <jonathanorellana@Jonathans-MacBook-Pro-2.local>
2025-01-21 00:48:56 -0500

    hh

--- src/main/java/edu/virginia/cs/intro/HelloWho.java
+++ src/main/java/edu/virginia/cs/intro/HelloWho.java
@@ -9,6 +9,6 @@ package edu.virginia.cs.intro;
 public class HelloWho {
     public static void main(String[] args) {
         String who = args[0];
-        System.out.println("Hello, " + who);
+        System.out.println("Helloooo, " + who);
     }
 }
